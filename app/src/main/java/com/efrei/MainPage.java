4777d8a6d876eb220c0daadbc06fc25d6c1e1253
Simone Melotti <simone.melotti96@gmail.com>
2024-05-20 15:06:00 +0200

    Updates main app

---
+++ app/src/main/java/com/efrei/MainPage.java
@@ -0,0 +1,18 @@
+package com.efrei;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainPage {
+
+	@FXML
+	private Button btnLogout;
+	
+	@FXML
+	public void userLogout(ActionEvent event) throws IOException {
+	}
+		
+}
